760932e7aa4f49bb5f3b76062e726951cd37f87c
rifatsamjid <rifattamim74328@gmail.com>
2025-11-13 17:34:14 +0600

    add enviroment variables

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwdoNXfHIVtI93limoDMgv82QV40qoxnk",
-  authDomain: "assignment-ten-76e7a.firebaseapp.com",
-  projectId: "assignment-ten-76e7a",
-  storageBucket: "assignment-ten-76e7a.firebasestorage.app",
-  messagingSenderId: "636351201120",
-  appId: "1:636351201120:web:21a76e569fdcd010232805"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
